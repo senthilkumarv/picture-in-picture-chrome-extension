664a8e46e3c967c17e3e611f9ba33fb999a8ffe0
Senthil Kumar <senthil.vijay.kumar@gmail.com>
2019-02-05 18:38:15 -0500

    Do not honor disablePictureInPicture flag

--- src/background.js
+++ src/background.js
@@ -20,7 +20,6 @@ if (!document.pictureInPictureEnabled) {
       (async () => {
         const videos = Array.from(document.querySelectorAll('video'))
             .filter(video => video.readyState != 0)
-            .filter(video => video.disablePictureInPicture == false)
             .sort((v1, v2) => {
               const v1Rect = v1.getClientRects()[0];
               const v2Rect = v1.getClientRects()[0];
